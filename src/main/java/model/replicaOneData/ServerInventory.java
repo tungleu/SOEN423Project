267e71edb3be21154837c5b5c832fea1d3270375
Kevin Tan <kevin.tan97@hotmail.com>
2020-11-11 20:23:12 -0500

    [#22] Adding data models for Kevin's codebase

---
+++ src/main/java/model/replicaOneData/ServerInventory.java
@@ -0,0 +1,50 @@
+package model.replicaOneData;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Kevin Tan 2020-09-21
+ */
+public class ServerInventory {
+
+    private final Map<String, Double> userBudgets;
+    private final Map<String, Item> inventoryCatalog;
+    private final Map<String, Queue<String>> itemWaitList;
+    private final Map<String, Map<String, List<PurchaseLog>>> userPurchaseLogs;
+    private final Set<String> foreignCustomers;
+    private final String serverName;
+
+    public ServerInventory(String serverName) {
+        this.inventoryCatalog = new ConcurrentHashMap<>();
+        this.itemWaitList = new ConcurrentHashMap<>();
+        this.userBudgets = new ConcurrentHashMap<>();
+        this.foreignCustomers = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        this.userPurchaseLogs = new ConcurrentHashMap<>();
+        this.serverName = serverName;
+    }
+
+    public Map<String, Item> getInventoryCatalog() {
+        return inventoryCatalog;
+    }
+
+    public Map<String, Queue<String>> getItemWaitList() {
+        return itemWaitList;
+    }
+
+    public Set<String> getForeignCustomers() {
+        return foreignCustomers;
+    }
+
+    public Map<String, Double> getUserBudgets() {
+        return userBudgets;
+    }
+
+    public Map<String, Map<String, List<PurchaseLog>>> getUserPurchaseLogs() {
+        return userPurchaseLogs;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+}
