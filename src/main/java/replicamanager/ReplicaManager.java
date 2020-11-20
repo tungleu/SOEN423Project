b20bc15e1630893ce58abcaefadc67227b63f4a6
Kevin Tan <kevin.tan97@hotmail.com>
2020-11-20 14:36:17 -0500

    [#55] Adding RM logger

--- src/main/java/replicamanager/ReplicaManager.java
+++ src/main/java/replicamanager/ReplicaManager.java
@@ -8,7 +8,11 @@ import replica.common.Replica;
 import util.MessageUtil;
 import util.ReplicaUtil;
 
+import java.io.IOException;
 import java.util.Stack;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
 
 import static common.ReplicaConstants.CLIENT_RM_CLUSTER;
 import static common.ReplicaConstants.REPLICA_RM_CLUSTER;
@@ -25,20 +29,25 @@ public class ReplicaManager {
     private final Stack<Long> failures;
 
     private Replica replicaServer;
+    private final Logger logger;
 
     public ReplicaManager(String name) throws Exception {
         this.name = name;
         clientRMChannel = new JChannel().setReceiver(clientRequestHandler()).setName(name);
         rmReplicaChannel = new JChannel().setReceiver(replicaRequestHandler()).setName(name);
         failures = new Stack<>();
+        logger = Logger.getLogger(name);
     }
 
     /**
      * Cold start both Replica and Replica Manager's clusters.
      */
     public void coldStart(String replicaOption) throws Exception {
+        initLogger();
         replicaName = replicaOption;
         rmReplicaChannel.connect(REPLICA_RM_CLUSTER);
+
+        logger.info(String.format("Starting replica %s.", replicaName));
         replicaServer = ReplicaUtil.startReplica(name, replicaName, null);
         clientRMChannel.connect(CLIENT_RM_CLUSTER);
     }
@@ -47,8 +56,10 @@ public class ReplicaManager {
         return msg -> {
             UDPRequestMessage udpRequestMessage = MessageUtil.messageToUDPRequest(msg);
             long failedSequenceNumber = Long.parseLong(udpRequestMessage.getParameters().get(0));
+            logger.info(String.format("Received error request from CORBA FE for message with sequence number %d", failedSequenceNumber));
 
             if (failures.peek() + 1 != failedSequenceNumber) {
+                logger.info(String.format("Error message with sequence number %d not consecutive, restarting count.", failedSequenceNumber));
                 failures.clear();
             }
 
@@ -56,6 +67,7 @@ public class ReplicaManager {
 
             // Check if it contains 3 consecutive fails
             if (failures.size() == 3) {
+                logger.info(String.format("3 consecutive fails for %s, begin replacement.", replicaName));
                 processFailureAndChoseReplica();
             }
         };
@@ -64,6 +76,7 @@ public class ReplicaManager {
     private Receiver replicaRequestHandler() {
         return msg -> {
             try {
+                logger.info(String.format("Data received for replacement replica %s, starting replica.", replicaName));
                 replicaServer = ReplicaUtil.startReplica(name, replicaName, msg);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -73,17 +86,28 @@ public class ReplicaManager {
 
     private void processFailureAndChoseReplica() {
         // Kill replica
+        logger.info(String.format("Killing replica %s.", replicaName));
         replicaServer.kill();
+
         // Fetch replacement replica
         replicaName = ReplicaUtil.chooseReplica(replicaName);
+        logger.info(String.format("Replacing killed replica with replica %s.", replicaName));
+
         Address destinationReplica = fetchAddressForDataTransfer(name, replicaName, rmReplicaChannel);
         try {
             // Request for data transfer
+            logger.info(String.format("Requesting data transfer for replica %s.", replicaName));
             rmReplicaChannel.send(createMessageFor(destinationReplica, new UDPRequestMessage(DATA_TRANSFER)));
             // Clear failures
             failures.clear();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void initLogger() throws IOException {
+        String logFile = System.getProperty("user.dir") + "/src/main/java/replicamanager/" + name + ".log";
+        Handler fileHandler =  new FileHandler(logFile, true);
+        this.logger.addHandler(fileHandler);
     }
 }
